4e98d5db3575cc08902e5eaff04562bbee47fbed
E Barr <emhb007@gmail.com>
2023-04-04 21:00:16 +0100

    Added custom exception class and with example usage.

---
+++ custom_exc_demo.py
@@ -0,0 +1,21 @@
+from person import Person, InsufficientSweetsException
+
+fred = Person("Fred","M")
+wilma = Person("Wilma","F",5)
+print(fred)
+print("Fred gets 10 sweets -->")
+fred.give_sweets(10)
+print(fred)
+print("Fred eats 6 sweets -->")
+fred.eat_sweets(6)
+print(fred)
+try:
+    print(wilma)
+    print("Wilma eats 6 sweets -->")
+    print(wilma.eat_sweets(6))
+except InsufficientSweetsException as e:
+    print(e)
+finally:
+    print(wilma)
+
+print("Done.")
---
+++ person.py
@@ -0,0 +1,38 @@
+class InsufficientSweetsException(Exception):
+    """Custom exception example inheriting from Exception class"""
+    def __init__(self, sweets):
+        self.msg = f"Not enough sweets to eat {sweets} sweets!"
+
+    def __str__(self):
+        return self.msg
+
+class Person:
+    """ Simple Person class to demo OO principles and a custom exception"""
+
+    def __init__(self, name, gender, sweets=0):
+        self._name = name
+        self._gender = gender.upper()
+        self._sweets = sweets
+
+    def __str__(self):
+        return f"Name: {self.get_name()} Gender: {self.get_gender()} has sweet count of: {self.get_sweets()}"
+
+    def get_name(self):
+        return self._name
+
+    def get_gender(self):
+        return self._gender
+
+    def give_sweets(self, num):
+        self._sweets += num
+        return
+
+    def get_sweets(self):
+        return self._sweets
+
+    def eat_sweets(self, num):
+        if num > self._sweets:
+            raise InsufficientSweetsException(num)
+        else:
+            self._sweets -= num
+            return f"{self.get_name()} now has {self.get_sweets()} sweets!"
